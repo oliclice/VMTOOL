1dc66b92867bdb32dabffbf05153de5b8cfbdb4e
oliclice <oliclice@qq.com>
2026-04-11 14:57:07 +0800

    修复QCheckBox未导入的问题

--- vm-tool/ui/gui/pyqt_app.py
+++ vm-tool/ui/gui/pyqt_app.py
@@ -11,7 +11,7 @@ from PyQt6.QtWidgets import (
     QTabWidget, QTableWidget, QTableWidgetItem, QPushButton, QLineEdit,
     QLabel, QDialog, QFormLayout, QComboBox, QMessageBox, QFileDialog,
     QTreeWidget, QTreeWidgetItem, QSplitter, QStatusBar, QMenuBar, QMenu,
-    QProgressDialog, QTextEdit, QGroupBox, QProgressBar
+    QProgressDialog, QTextEdit, QGroupBox, QProgressBar, QCheckBox
 )
 from PyQt6.QtCore import Qt, QSortFilterProxyModel, QThread, pyqtSignal
 from PyQt6.QtGui import QAction, QIcon, QFont, QColor, QStandardItemModel, QStandardItem, QPalette
